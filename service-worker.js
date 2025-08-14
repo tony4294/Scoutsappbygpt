e70a6f44571d5dff1745414e6bf136b05132b699
tony4294 <tonygeorges2004@gmail.com>
2025-08-13 23:33:13 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,20 @@
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('v1').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/app.js',
-        '/manifest.json'
-      ]);
-    })
+    caches.open('ultimate-scout-cache-v1').then(cache => cache.addAll([
+      '/',
+      '/index.html',
+      '/style.css',
+      '/app.js',
+      '/manifest.json'
+    ]))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
 });
